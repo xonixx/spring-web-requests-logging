1f0cd917171044aaead8a6f426d9a43ca9141eb7
Alexey Kopylov <tonn.tamerlan@gmail.com>
2018-11-13 17:25:32 +0200

    add bean to configuration

--- src/main/java/com/cmlteam/web/LogRestRequestWebInterceptorConfiguration.java
+++ src/main/java/com/cmlteam/web/LogRestRequestWebInterceptorConfiguration.java
@@ -3,6 +3,7 @@ package com.cmlteam.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -19,6 +20,11 @@ public class LogRestRequestWebInterceptorConfiguration extends WebMvcConfigurerA
   public LogRestRequestWebInterceptorConfiguration(
       LogRestRequestWebInterceptor logRestRequestWebInterceptor) {
     this.logRestRequestWebInterceptor = logRestRequestWebInterceptor;
+  }
+
+  @Bean
+  public LogRestRequestWebInterceptor logRestRequestWebInterceptor(){
+    return new LogRestRequestWebInterceptor();
   }
 
   @Override
